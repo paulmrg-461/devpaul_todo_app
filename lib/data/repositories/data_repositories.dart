229950cdac0faee8acd05e9bcaf5249172aef20c
paulmrg-461 <paulmrg461@gmail.com>
2025-03-31 12:53:53 -0500

    Feat: Tasks: Data: added model, datasource, repository_impl

--- lib/data/repositories/data_repositories.dart
+++ lib/data/repositories/data_repositories.dart
@@ -1,2 +1,3 @@
 export 'package:devpaul_todo_app/data/repositories/user_repository_impl.dart';
 export 'package:devpaul_todo_app/data/repositories/auth_repository_impl.dart';
+export 'package:devpaul_todo_app/data/repositories/task_repository_impl.dart';
